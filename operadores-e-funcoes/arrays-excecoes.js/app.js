d38a822c48b3bc98fa3fbfe9345ff50c96d82af0
Luana Ferreira <luanaferreiraservices@gmail.com>
2022-07-09 19:45:56 -0300

    explanation

--- operadores-e-funcoes/arrays-excecoes.js/app.js
+++ operadores-e-funcoes/arrays-excecoes.js/app.js
@@ -26,6 +26,19 @@ try {
     
     const listaDeProdutosSolicitadosNaoDisponiveis = listaArgs.filter((argumento) => {
         return !listaDeProdutosDisponiveis.find((produto) => produto === argumento);
+        /*
+        Dentro do contexto do filter, este código vai adicionar o produto na lista dos não disponíveis
+         se não encontrar um produto com o mesmo nome na lista de produtos disponíveis,
+
+        Em um dos casos, ele só vai adicionar se não tiver nenhum produto que seja igual. Você está pegando
+         o resultado e vendo, se não encontrou nenhum adiciona na lista dos não disponíveis.
+        No outro, você vai adicionar se existir algum produto que seja difernete. Se a pessoa está adicionando
+         laranja na lista de produtos, ele vai adicionar ela na lista de produtos não disponíveis se existir algum
+         produto que não seja uma laranja.
+
+        Geralmente esse ! antes de alguma coisa qUer dizer: "se essa coisa não existir, faz isso". O find, quando não 
+         acha nada, retorna undefined quando você usa o operador ! no undefined o resultado é true.
+        */
     });
     
     listaDeProdutosSolicitadosNaoDisponiveis.forEach((produto) => console.log(`Este produto nós não temos: ${produto}`));
