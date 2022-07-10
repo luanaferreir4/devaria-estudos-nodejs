61a0e7af2ca88fc3496c5ca9aabb7331fb112038
Luana Ferreira <luanaferreiraservices@gmail.com>
2022-07-09 23:22:02 -0300

    saving app.js file

---
+++ 03.gestao-de-dependencias-e-frameworks/hello-npm/app.js
@@ -0,0 +1,4 @@
+const moment = require('moment');
+
+console.log(moment().toString());
+// moment.js: A JavaScript date library for parsing, validating, manipulating, and formatting dates. npm i moment
